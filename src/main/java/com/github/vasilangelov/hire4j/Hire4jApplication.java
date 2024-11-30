229cc28f4c01a51e850cf474afe4f743a60406d3
vasilangelov <86876332+vasilangelov@users.noreply.github.com>
2024-11-30 16:56:47 +0200

    Add Spring Boot template

---
+++ src/main/java/com/github/vasilangelov/hire4j/Hire4jApplication.java
@@ -0,0 +1,13 @@
+package com.github.vasilangelov.hire4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hire4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hire4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vasilangelov/hire4j/Hire4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vasilangelov.hire4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hire4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
